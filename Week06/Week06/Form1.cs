a3d56e5bf983bf2a3767a1a039d9cfdabbc70429
Ákos Tóth <fehsoly@gmail.com>
2021-10-29 10:28:33 +0200

    6. feladat

--- Week06/Week06/Form1.cs
+++ Week06/Week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using Week06.Entities;
 using Week06.MnbServiceReference;
@@ -27,7 +28,26 @@ namespace Week06
             dataGridView1.DataSource = Rates;
             string xmlstring = GetExchangeRatesRequest();
             Feldolgozas(xmlstring);
+            Diagram();
             
+        }
+
+        private void Diagram()
+        {
+            chartRateData.DataSource = Rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
         public string  GetExchangeRatesRequest()
