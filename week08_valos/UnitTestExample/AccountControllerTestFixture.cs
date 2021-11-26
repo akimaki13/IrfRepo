19fcc5dd7f597585ce03707675f2327ff55e374a
Ákos Tóth <fehsoly@gmail.com>
2021-11-26 14:36:11 +0100

    HappyPath

--- week08_valos/UnitTestExample/AccountControllerTestFixture.cs
+++ week08_valos/UnitTestExample/AccountControllerTestFixture.cs
@@ -47,5 +47,27 @@ namespace UnitTestExample
             return kisbetu.IsMatch(password) && nagybetu.IsMatch(password) && szam.IsMatch(password) && nyolchosszu.IsMatch(password);
         }
 
+        [
+            Test,
+            TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+        ]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.Register(email, password);
+
+            // Assert
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+
+
     }
+
+
 }
