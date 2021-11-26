2cc7419d2e2835879368c010c5e3fd065adc7b86
Ákos Tóth <fehsoly@gmail.com>
2021-11-26 14:08:53 +0100

    Validálás teszt kifejtés

--- week08_valos/UnitTestExample/AccountControllerTestFixture.cs
+++ week08_valos/UnitTestExample/AccountControllerTestFixture.cs
@@ -11,7 +11,14 @@ namespace UnitTestExample
         [Test]
         public void TestValidateEmail(string email, bool expectedResult)
         {
-            
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
