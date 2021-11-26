3b6e2c5c5a904245e7cd74c02027807102494cb4
Ákos Tóth <fehsoly@gmail.com>
2021-11-26 14:02:42 +0100

    Teszt függvény

---
+++ week08_valos/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestExample.Test
+{
+    class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult) 
+        {
+
+
+        }
+
+
+
+    }
+}
